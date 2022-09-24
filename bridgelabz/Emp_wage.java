23171ae32dd1e69a62ccf46ea1d221f7b7eb4602
Sriganth <86834622+Sriganth145@users.noreply.github.com>
2022-09-24 12:43:24 +0530

    case Emp_wage

--- bridgelabz/Emp_wage.java
+++ bridgelabz/Emp_wage.java
@@ -13,16 +13,21 @@ public class Emp_wage {
 		int Wage_per_hour = 20;
 		int Full_hour ;
 		
-		if(Ran == 2) {
-			Full_hour =8;
-			System.out.println("Employee is present");
-		}else if (Ran == 1) {
-			Full_hour = 4;
-			System.out.println("Employee is Part time");
-		}
-		else {
-				Full_hour = 0;
-				System.out.println("Employee is absent");
+		switch (Ran) {
+		
+		case 0 : Full_hour =0;
+					System.out.println("Employee is absent");
+					break;
+					
+					
+		case 1 : Full_hour = 4;
+					System.out.println("Employee is Part time");
+					break;
+					
+		case 2 : 
+				Full_hour = 8;
+				System.out.println("Employee is Present");
+				break;
 			
 		}
 			int Emp_wage_perday = Full_hour*Wage_per_hour ;
