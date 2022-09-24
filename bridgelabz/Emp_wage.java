36c688ff2de18826e51c9a51ba93c74c59c2caf8
Sriganth145 <sriganth9688@gmail.com>
2022-09-24 11:36:45 +0530

    added : Employee wage

--- bridgelabz/Emp_wage.java
+++ bridgelabz/Emp_wage.java
@@ -1,10 +1,28 @@
 package bridgelabz;
 
+import java.util.Random;
+
 public class Emp_wage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Random Input = new Random();
+		
+		int Ran = Input.nextInt(2);
+		
+		int Wage_per_hour = 20;
+		int Full_hour ;
+		
+		if(Ran == 1) {
+			Full_hour =8;
+			System.out.println("Employee is present");
+		}else {
+				Full_hour = 0;
+				System.out.println("Employee is absent");
+			
+		}
+			int Emp_wage_perday = Full_hour*Wage_per_hour ;
+			System.out.println(Emp_wage_perday);
 	}
 
 }
